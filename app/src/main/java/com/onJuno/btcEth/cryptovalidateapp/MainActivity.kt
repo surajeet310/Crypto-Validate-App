bc35cf25ab7800308769fd6f1c44ea8124440243
surajeet310 <surajeet310@gmail.com>
2022-05-07 01:35:47 +0530

    Added QR code scanning functionality and retrieving the scanned QR code data. Some design changes are also made

--- app/src/main/java/com/onJuno/btcEth/cryptovalidateapp/MainActivity.kt
+++ app/src/main/java/com/onJuno/btcEth/cryptovalidateapp/MainActivity.kt
@@ -1,11 +1,111 @@
 package com.onJuno.btcEth.cryptovalidateapp
 
+import android.Manifest
+import android.app.Activity
+import android.content.Intent
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Observer
+import com.google.android.material.card.MaterialCardView
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var scanBtcBtn:Button
+    private lateinit var scanEthBtn:Button
+    private lateinit var validateBtn:Button
+    private lateinit var shareBtn:Button
+    private lateinit var addressText:TextView
+    private lateinit var resultCardView:MaterialCardView
+    private var btcAddress:String = ""
+    private var ethAddress:String = ""
+    private val CAMERA_REQUEST_CODE = 100
+    private var isPermitted = MutableLiveData<Boolean>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        scanBtcBtn = findViewById(R.id.scan_btc_btn)
+        scanEthBtn = findViewById(R.id.scan_eth_btn)
+        addressText = findViewById(R.id.qr_code_data)
+        resultCardView = findViewById(R.id.result_card_view)
+        val startScannerActivityForBtc = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
+            if (it.resultCode == Activity.RESULT_OK){
+                if (it.data != null){
+                    btcAddress = it.data!!.getStringExtra("data").toString()
+                }
+                else{
+                    btcAddress = "None"
+                }
+                resultCardView.visibility = View.VISIBLE
+                addressText.text = "Scanned BTC Address: "+btcAddress
+            }
+        }
+        val startScannerActivityForEth = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
+            if (it.resultCode == Activity.RESULT_OK){
+                if (it.data != null){
+                    ethAddress = it.data!!.getStringExtra("data").toString()
+                }
+                else{
+                    ethAddress = "None"
+                }
+                resultCardView.visibility = View.VISIBLE
+                addressText.text = "Scanned ETH Address: "+ethAddress
+            }
+        }
+        scanBtcBtn.setOnClickListener {
+            checkPermissions()
+            isPermitted.observe(this, Observer { status->
+                if (status){
+                    startScannerActivityForBtc.launch(Intent(this,ScannerActivity::class.java))
+                }
+                else{
+                    Snackbar.make(it,"Permission for Camera is required to scan QR",Snackbar.LENGTH_SHORT).show()
+                }
+            })
+        }
+        scanEthBtn.setOnClickListener {
+            checkPermissions()
+            isPermitted.observe(this, Observer { status->
+                if (status){
+                    startScannerActivityForEth.launch(Intent(this,ScannerActivity::class.java))
+                }
+                else{
+                    Snackbar.make(it,"Permission for Camera is required to scan QR",Snackbar.LENGTH_SHORT).show()
+                }
+            })
+        }
+    }
+
+    private fun checkPermissions(){
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_DENIED){
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.CAMERA),CAMERA_REQUEST_CODE)
+        }
+        else{
+            isPermitted.postValue(true)
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if(requestCode == CAMERA_REQUEST_CODE){
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                isPermitted.postValue(true)
+            }
+            else{
+                isPermitted.postValue(false)
+            }
+        }
     }
 }
--- app/src/main/java/com/onJuno/btcEth/cryptovalidateapp/ScannerActivity.kt
+++ app/src/main/java/com/onJuno/btcEth/cryptovalidateapp/ScannerActivity.kt
@@ -1,11 +1,58 @@
 package com.onJuno.btcEth.cryptovalidateapp
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import com.budiyev.android.codescanner.AutoFocusMode
+import com.budiyev.android.codescanner.CodeScanner
+import com.budiyev.android.codescanner.CodeScannerView
+import com.budiyev.android.codescanner.DecodeCallback
+import com.budiyev.android.codescanner.ErrorCallback
 
 class ScannerActivity : AppCompatActivity() {
+    private lateinit var codeScannerView: CodeScannerView
+    private lateinit var codeScanner:CodeScanner
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_scanner)
+        codeScannerView = findViewById(R.id.scanner_view)
+        codeScanner = CodeScanner(this,codeScannerView)
+        codeScanner.camera = CodeScanner.CAMERA_BACK
+        codeScanner.formats = CodeScanner.ALL_FORMATS
+        codeScanner.autoFocusMode = AutoFocusMode.SAFE
+        codeScanner.isAutoFocusEnabled = true
+        codeScanner.isFlashEnabled = false
+
+        codeScanner.decodeCallback = DecodeCallback {
+            val intent = Intent().apply {
+                putExtra("data",it.text.toString())
+            }
+            setResult(Activity.RESULT_OK,intent)
+            finish()
+        }
+
+        codeScanner.errorCallback = ErrorCallback {
+            runOnUiThread {
+                Toast.makeText(this,"Error occurred in opening camera",Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        codeScannerView.setOnClickListener {
+            codeScanner.startPreview()
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        codeScanner.startPreview()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        codeScanner.releaseResources()
     }
 }
